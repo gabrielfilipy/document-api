2473629768a9c0d7a68e10a606b2683ee2372812
marcusviniciusbsouza <lorde9030@gmail.com>
2024-05-09 18:33:51 -0400

    Correção nO DTO document

--- src/main/java/com/br/api/v1/model/DocumentModel.java
+++ src/main/java/com/br/api/v1/model/DocumentModel.java
@@ -1,6 +1,6 @@
 package com.br.api.v1.model;
 
-import com.br.api.v1.model.input.ModelModelInput;
+
 import lombok.*;
 
 @Getter
@@ -8,7 +8,6 @@ import lombok.*;
 public class DocumentModel {
 
 	private Long documentId;
-	private ModelModelInput model;
 	private Long subscritorId;
 	private String descricao;
 	private MobilModel mobil;
