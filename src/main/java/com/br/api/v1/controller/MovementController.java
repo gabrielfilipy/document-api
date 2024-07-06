882e875fc3b3fc9ffd0266ad03c3edc25f6030f7
yasmimsoaresverissimo <yasmim.soares.verissimo@gmail.com>
2024-07-06 09:17:13 -0400

    Service-Excluir-Cossignatario

--- src/main/java/com/br/api/v1/controller/MovementController.java
+++ src/main/java/com/br/api/v1/controller/MovementController.java
@@ -99,5 +99,10 @@ public class MovementController {
 		movimentacaoAssinadaModel.setMobilModel(mobilModel);
 		return ResponseEntity.status(HttpStatus.OK).body(movimentacaoAssinadaModel);
 	}
-
-} 
+	@DeleteMapping("/excluir-movimentacao/{siglaMobil}/{movimentacaoId}")
+    public ResponseEntity<Void> verificarEExcluirMovimentacao(
+    		@PathVariable(name = "siglaMobil") String siglaMobil, @PathVariable(name = "movimentacaoId") Long movimentacaoId) {
+        movementService.verificarEExcluirMovimentacao(siglaMobil, movimentacaoId);
+        return ResponseEntity.noContent().build();
+    }
+}
